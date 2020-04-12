d6e42b8c9a18bee560effca4cd2964b46e12c4ea
dipeshkumarsah98 <63381568+dipeshkumarsah98@users.noreply.github.com>
2020-04-12 08:49:06 +0545

    Update urls.py

--- project/mysite/urls.py
+++ project/mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from my_base.project.first_app import views
+from first_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
